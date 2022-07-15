cf12d226d5619f1e167756c9060d931a99a214d1
Vishruta_Patil <vishrutapatil2106@gmail.com>
2022-07-15 13:42:41 +0530

    removed commented code

--- src/components/UserSidebar/UserCard.tsx
+++ src/components/UserSidebar/UserCard.tsx
@@ -36,11 +36,6 @@ const unFollowHandler = () => {
     setFollowStatus((prev:Boolean) => !prev)
 } 
 
-// useEffect(() => {
-//   setFollowStatus(authItem?.following?.followingBy.some((user:any) => user.id === item.id))
-// }, [authItem])
-
-
     return (
       <div className="flex justify-between items-center mb-6">
         <div className="flex items-center space-x-3">
